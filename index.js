6e0a164b8197b18cd1b063094246d8f24261f344
mykola-lp <mykola.sukhorab@gmail.com>
2026-08-26 10:56:50 +0300

    determine which HTML file to serve based on the path

--- index.js
+++ index.js
@@ -14,6 +14,26 @@ const server = http.createServer((req, res) => {
   //   '/about'       -> about.html
   //   '/contact-me'  -> contact-me.html
   //   any other path -> 404.html
+  let fileName;
+
+  switch (url) {
+    case '/':
+      fileName = 'index.html';
+      break;
+
+    case '/about':
+      fileName = 'about.html';
+      break;
+
+    case '/contact-me':
+      fileName = 'contact-me.html';
+      break;
+
+    default:
+      fileName = '404.html';
+  }
+
+  const filePath = path.join(__dirname, 'pages', fileName);
 
   // TODO 3: Read the corresponding file (fs.readFile)
   //   https://nodejs.org/learn/manipulating-files/reading-files-with-nodejs
